159e6b14ae17c3d9b5f3f869d621953fdf88a531
SyedShahbazHussain <syedshahbaz.hussain@outlook.com>
2019-02-11 23:14:24 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
-import SignIn from './pages/SignInForm';
+import logo from './logo.svg';
 import './App.css';
- 
 
 class App extends Component {
-  render(){
+  render() {
     return (
-
- 
- <Router>
-  <Route exact path="/" component={SignIn}>
-  </Route>
-  <Route path="/sign-In">
-   <h1> There is SignIn page
-   </h1>
-  </Route>
-
-  </Router>
-   
-
-   )
-  }    
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
