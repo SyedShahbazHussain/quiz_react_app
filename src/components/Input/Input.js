d68c65c2205b473d435fba0d5433a674db5cfb8b
SyedShahbazHussain <syedshahbaz.hussain@outlook.com>
2019-02-16 23:31:54 +0500

    Continue

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,31 +1,31 @@
 import React from 'react';
 
-const Input = (props) =>{
+const Input = (props) => {
     const {
         type, value, id, name, label, placeholder, errors, onChange } = props;
-        return(
-            <div className="inputContainer">
+    return (
+        <div className="inputContainer">
             <div className="inputWrapper">
-            <label htmlFor={id}>
-            {label}
-            <input
-             type={type}
-             value={value}
-             id={id}
-             name={name}
-             placeholder={placeholder}
-             onChange={(ev)=>onChange(ev)} 
-             /> 
-             </label>  
-             </div>
-             
-             {errors.errorsObj[name] && <div className="error-wrapper">
-             <p className="error"> {errors.errorObj[name].message}</p>
-             </div>
-             }
-             </div>
-             
-        )
-        
-    }
-export {Input}
+                <label htmlFor={id}>
+                    {label}
+                    <input
+                        type={type}
+                        value={value}
+                        id={id}
+                        name={name}
+                        placeholder={placeholder}
+                        onChange={(ev) => onChange(ev)}
+                    />
+                </label>
+            </div>
+
+            {errors.errorsObj[name] && <div className="error-wrapper">
+                <p className="error"> {errors.errorObj[name].message}</p>
+            </div>
+            }
+        </div>
+
+    )
+
+}
+export default Input
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,39 +1,41 @@
 import React, { Component } from 'react';
-
+import Input from '../Input/Input';
 class Login extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
-            users:[
-                {id: 0, name: "admin", pass:"admin"},
-                {id: 1, name:"sha", pass:"123"}
-              ],
-              isAuth: false,
-              currentUser: null,
-              errors:{
-                  hasError: false,
-                  errorsObj:{}
+        this.state = {
+            users: [
+                { id: 0, name: "admin", pass: "admin" },
+                { id: 1, name: "sha", pass: "123" }
+            ],
+            isAuth: false,
+            currentUser: null,
+            errors: {
+                hasError: false,
+                errorsObj: {}
 
-              },
-              name: "",
-              pass: "",
-         }
+            },
+            name: "",
+            pass: "",
+        }
     }
-    render() { 
-        const {name, errors, pass} = this.state;
-        return ( 
-        <div>
-            <form onSubmit={(ev) => this.onSubmit(ev)}>
-            
-            <input type="text" value={name} name="name" id="name" label="Username" placeholder="Enter your name here"
-            onChange={(ev)=> this.setState({[ev.target.name]:ev.target.value})} errors={errors}
-            />   
-            <input type="password" value={pass} name="pass" id="pass" label="Password" placeholder="Enter your password here"
-            onChange={(ev)=> this.setState({[ev.target.name]:ev.target.value})} errors={errors}
-            />  
-            </form>
-        </div> );
+    render() {
+        const { name, errors, pass } = this.state;
+        return (
+            <div>
+                <form onSubmit={(ev) => this.onSubmit(ev)}>
+                    <Input type="text" value={name} name="name" id="name" label="Username" placeholder="Enter your name here"
+                        onChange={(ev) => this.setState({ [ev.target.name]: ev.target.value })} errors={errors}
+                    />
+                    <Input type="password" value={pass} name="pass" id="pass" label="Password" placeholder="Enter your password here"
+                        onChange={(ev) => this.setState({ [ev.target.name]: ev.target.value })} errors={errors}
+                    />
+                </form>
+
+
+
+            </div>);
     }
 }
- 
+
 export default Login;
